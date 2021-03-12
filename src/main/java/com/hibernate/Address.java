806aaf43ffa0c1ef76f4599101419a93afebf0f9
sohail-dev786 <sohailss4444@gmail.com>
2021-03-12 20:58:36 +0530

    Created a New Class (Address.java) in that All Basic Commonly use Hibernate Annotation Use

---
+++ src/main/java/com/hibernate/Address.java
@@ -0,0 +1,136 @@
+package com.hibernate;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "Student_Address")
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Address_ID")
+	private int addressId;
+	
+	@Column(length = 50,name = "STREET")
+	private String street;
+	
+	@Column(length = 30,name = "CITY")
+	private String city;
+	
+	@Column(length = 50,name = "Is_Open")
+	private boolean isOpen;
+	
+	@Transient
+	private boolean x;
+	
+	@Column(length = 50,name = "COUNTRY")
+	private String country;
+	
+	@Column(name = "Added_Date")
+	@Temporal(TemporalType.DATE)
+	private Date addedDate;
+	
+	@Lob
+	private byte[] image;
+	
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Address(int addressId, String street, String city, boolean isOpen, boolean x, String country, Date addedDate,
+			byte[] image) {
+		super();
+		this.addressId = addressId;
+		this.street = street;
+		this.city = city;
+		this.isOpen = isOpen;
+		this.x = x;
+		this.country = country;
+		this.addedDate = addedDate;
+		this.image = image;
+	}
+
+	public int getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public boolean isOpen() {
+		return isOpen;
+	}
+
+	public void setOpen(boolean isOpen) {
+		this.isOpen = isOpen;
+	}
+
+	public boolean isX() {
+		return x;
+	}
+
+	public void setX(boolean x) {
+		this.x = x;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public Date getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+	
+	
+	
+
+	
+	
+	
+}
--- src/main/java/com/hibernate/App.java
+++ src/main/java/com/hibernate/App.java
@@ -1,5 +1,9 @@
 package com.hibernate;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -7,7 +11,7 @@ import org.hibernate.cfg.Configuration;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "App Started!" );
         
@@ -18,6 +22,8 @@ public class App
         System.out.println(sessionFactory.isOpen());
         System.out.println(sessionFactory.isClosed());
         
+        
+        //CREATING OBJECT OF STUDENT
         Student  student=new Student();
         
         student.setId(101);
@@ -26,16 +32,36 @@ public class App
         student.setCity("MUMBAI");
         
         
-        Session session=sessionFactory.openSession();
+        //CREATING OBJECT OF ADDRESS
         
+        Address add=new Address();
+        
+        add.setStreet("Jogeshwari");
+        add.setCity("Mumbai");
+        add.setCountry("INDIA");
+        add.setX(true);
+        add.setAddedDate(new Date());
+        
+        //INSERTING IMAGES INTO DATABSES
+        //FOR THAT WE HAVE TO READ THE PATH  THEN STORE INTO THE DATABASE
+        
+        FileInputStream fis=new FileInputStream("src/main/java/Sohail_Dp.jpg");
+        
+        byte[] data=new byte[fis.available()];
+        fis.read(data);
+        add.setImage(data);
+        
+        
+        Session session=sessionFactory.openSession();       
         Transaction transaction=session.beginTransaction();
         
         session.save(student);
+        session.save(add);
         
         transaction.commit();
-        
         session.close();
         
+        System.out.println("DONE !!!");
         
         
         
